e13f84716ce871d97f8d7dfb243b319a8571aade
Henry Ferrao <56619373+hgferrao@users.noreply.github.com>
2021-11-01 23:05:18 +0530

    Add files via upload

---
+++ script4.js
@@ -0,0 +1,33 @@
+/*
+function addition(a, b) {
+  return a + b;
+}
+console.log(addition(2, 3)); //, 5)
+console.log(addition(-3, -6)); //, -9)
+console.log(addition(7, 3)); //, 10)
+console.log(addition(88, 2)); //, 90)
+
+
+//Convert Minutes into Seconds
+function convert(minutes) {
+  return minutes * 60;
+}
+console.log(convert(6)); //, 360)
+console.log(convert(4)); //, 240)
+console.log(convert(8)); //, 480)
+console.log(convert(60)); //, 3600)
+
+// Length of a Nested Array
+function getLength(arr) {
+  return arr.flat(Infinity).length;
+}
+console.log(getLength([1, [2, 3]])); //, 3);
+console.log(getLength([1, [2, [3, 4]]])); //, 4);
+console.log(getLength([1, [2, [3, [4, [5, 6]]]]])); //, 6);
+console.log(getLength([1, 7, 8])); //, 3);
+console.log(getLength([2])); //, 1);
+console.log(getLength([2, [3], 4, [7]])); //, 4);
+console.log(getLength([2, [3, [5, 7]], 4, [7]])); //, 6);
+console.log(getLength([2, [3, [4, [5]]], [9]])); //, 5);
+console.log(getLength([])); //, 0);
+*/
